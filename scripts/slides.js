879ff5110a1af225810da7ec216375abf3313e4d
Joe Latessa <latessa@users.noreply.github.com>
2023-05-02 14:20:57 -0400

    updated landing page images sizes & slide duration
    increased size of rotating slides, increased
    duration of each slide, reduced size of
    institutions emblem

--- scripts/slides.js
+++ scripts/slides.js
@@ -15,5 +15,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // advance image every 2 seconds
+  setTimeout(showSlides, 10000); // advance image every 2 seconds
 }
