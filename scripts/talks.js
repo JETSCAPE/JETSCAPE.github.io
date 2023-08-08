77e3809af5363d1f6abcb535db7b1d46ecb2a663
Joe Latessa <latessa@users.noreply.github.com>
2023-08-08 12:26:10 -0400

    updated JavaScript files
    updated JavaSCript files so that anchor tags aren't
    written if no link is given in the .json data

--- scripts/talks.js
+++ scripts/talks.js
@@ -13,11 +13,17 @@ let tableID = "#talkTable";
 let makeTable = ()=> {
   createTable(dataFile, tableID, (arr)=> {
     for (let i = 0; i < arr.length; i++) {
-      let temp = "<a style=\"color:#333747\" href=\"" + arr[i][1] + "\">" + arr[i][0] + "</a>";
-      arr[i][0] = temp;
+
+      if (arr[i][1] != "") {
+        arr[i][0] = "<a style=\"color:#333747\" href=\"" + arr[i][1] + "\">" + arr[i][0] + "</a>";
+      }
+
       arr[i].splice(1, 1); // deleting the old element 1
-      temp = "<a style=\"color:#333747\" href=\"" + arr[i][4] + "\">" + arr[i][3] + "</a>";
-      arr[i][3] = temp;
+
+      if (arr[i][4] != "") {
+        arr[i][3] = "<a style=\"color:#333747\" href=\"" + arr[i][4] + "\">" + arr[i][3] + "</a>";
+      }
+
       arr[i].splice(4, 1); // deleting the old element 4
     }
     return arr;
