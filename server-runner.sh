dadfac5d22981d55b82c0a9bf1df2cd1347a97aa
Haydar Mehryar <haydarmehryar@gmail.com>
2023-11-09 09:55:25 -0500

    init responsive @hmehryar

---
+++ server-runner.sh
@@ -0,0 +1,7 @@
+# #!binbash
+​
+# This script is used to deploy the test version of the application to the test server.
+​
+# copy all files and subdirectories to the test server
+# int XAMP Stack's htdocs folder
+cp -r * /mnt/c/MAMP/htdocs/JETSCAPE.github.io/
